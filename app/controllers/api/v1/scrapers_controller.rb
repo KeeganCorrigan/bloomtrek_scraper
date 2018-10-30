ccb17eda7b4d02454fa1a109c8078e54a9903f6e
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-30 15:29:34 -0600

    Refactor scraperscontroller

--- app/controllers/api/v1/scrapers_controller.rb
+++ app/controllers/api/v1/scrapers_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::ScrapersController < ApplicationController
+  before_action :set_website, only: [:index]
+
   def index
-    website = params[:website] + "&parkname=" + params[:parkname].gsub(" ", "+")
-    scraper = WebScraper.new(website)
-    plant_info = scraper.get_plant_info
+    scraper = WebScraper.new(@website)
+    scraper.get_plant_info
     cleaned_data = FlowerValidator.new(scraper.flower_array).clean_data
 
     render json: cleaned_data
+  end
+
+  private
+
+  def set_website
+    @website = params[:website] + "&parkname=" + params[:parkname].gsub(" ", "+")
   end
 end
